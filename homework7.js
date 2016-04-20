af99d4d6c496cf47efd0f41e024469a0e971ddd6
sarahalfayez <sarahalfayez@csu.fullerton.edu>
2016-04-20 14:03:05 -0700

    final homework7.js

---
+++ homework7.js
@@ -0,0 +1,92 @@
+var express = require("express"),
+    http = require("http"),
+    bodyParser = require("body-parser"),
+    mongoDB = require('mongodb'),
+    MongoClient = mongoDB.MongoClient,
+    app = express();
+
+
+http.createServer(app).listen(3000);
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+var url = "mongodb://localhost/hw7";
+var db, pathname, link;
+
+MongoClient.connect(url, function(err, database) {
+    if (err) {
+        console.log("Unable to connect to the mongoDB server. Error:", err);
+    } else {
+        console.log("successfully connected to the database");
+        db = database;
+
+    }
+});
+//GET links
+app.get("/links", function(req, res) {
+    console.log("get /links");
+    db.collection("links").find({}).toArray(function(err, doc) {
+        if (err) {
+            console.log("Unable to get the links. Error:", err);
+        } else {
+            res.json(doc);
+        }
+    });
+
+});
+//POST /links
+app.post("/links", function(req, res) {
+    var Title = req.body.title;
+    var Links = req.body.link;
+    var Clicks = 0;
+    if (!(req.body.title || req.body.link)) {
+        console.log("Invalid user input", "Must provide a title or link.", 400);
+    }
+    db.collection("links").insert({
+        title: Title,
+        link: Links,
+        clicks: Clicks
+    }, function(err, doc) {
+        if (err) {
+            console.log("Failed to create new links. Error:", err);
+        } else {
+            res.status(201).json(doc.ops[0]);
+            console.log(Title + "  link added");
+        }
+    });
+});
+// GET /click/:title
+app.get("/click/:" + pathname, function(req, res) {
+    console.log(req.originalUrl);
+    var pathArray = req.originalUrl.split('/');
+    var path = pathArray[2];
+    console.log(path);
+    db.collection("links").findOneAndUpdate({
+        "title": path
+    }, {
+        $inc: {
+            "clicks": 1
+        }
+    }, {
+        returnOriginal: false,
+        upsert: true
+    }, function(err, doc) {
+        if (err) {
+            console.log("Failed to get link. Error:", err);
+        } else {
+            console.log("increment success");
+            db.collection("links").distinct("link", {
+                "title": path
+            }).then(function(link) {
+                console.log(link);
+                res.writeHead(302, {
+                    "Location": link
+                });
+
+                res.end();
+            });
+        }
+    });
+});
